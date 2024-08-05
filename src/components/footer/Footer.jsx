dc15a558c664faddfa80e7f0d6e7e09f12cc73bd
Ferras Ibrahim <ibrahimferrasdev@gmail.com>
2024-08-05 12:38:20 +0100

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,25 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer className="flex">
+      <ul className="flex">
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Projects</a>
+        </li>
+        <li>
+          <a href="">Speaking</a>
+        </li>
+        <li>
+          <a href="">Uses</a>
+        </li>
+      </ul>
+      <p>&#169; Ibrahim FRS. All right reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
